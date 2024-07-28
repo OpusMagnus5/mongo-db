433e868265b2a10f07a5f6beb07666d1ccda2df6
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-07-28 10:11:23 +0200

    JAVA - insert, insertMany

--- src/main/java/pl/bodzioch/damian/Main.java
+++ src/main/java/pl/bodzioch/damian/Main.java
@@ -6,13 +6,30 @@ import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.BsonObjectId;
+import org.bson.BsonValue;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
+
+    static MongoClient mongoClient = createMongoClient();
+    static MongoCollection<Document> collection = getCollection();
+
     public static void main(String[] args) {
-        MongoClient mongoClient = createMongoClient();
         logInfo(mongoClient);
     }
 
@@ -29,9 +46,38 @@ public class Main {
         return MongoClients.create(settings);
     }
 
+    // wyciąganie kolekcji
+    private static MongoCollection<Document> getCollection() {
+        MongoDatabase database = mongoClient.getDatabase("name_of_database");
+        return database.getCollection("name_of_collection");
+    }
+
     private static void logInfo(MongoClient mongoClient) {
         mongoClient.listDatabases().into(new ArrayList<>()).stream()
                 .map(Document::toJson)
                 .forEach(System.out::println);
+    }
+
+    // tworzenie dokumentu do zapisu
+    private static Document exampleNewDocument() {
+        return new Document("_id", new ObjectId())
+                .append("name", "Damian")
+                .append("number", 12345)
+                .append("date", Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .append("address", new Document().append("city", "Katowice").append("street", "Albatrosów")); // zagnieżdżony dokument
+    }
+
+    // zapis pojedynczego dokumentu
+    private static void insertDocument() {
+        InsertOneResult insertOneResult = collection.insertOne(exampleNewDocument());
+        BsonValue insertedId = insertOneResult.getInsertedId();
+    }
+
+    // zapis wielu dokumentów
+    private static void insertManyDocuments() {
+        List<Document> documents = Stream.of(1, 2, 3).map(element -> exampleNewDocument()).toList();
+        InsertManyResult insertManyResult = collection.insertMany(documents);
+        List<BsonValue> ids = insertManyResult.getInsertedIds().values().stream().toList();
+        BsonObjectId objectId = ids.getFirst().asObjectId();
     }
 }
